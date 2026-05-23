e214623c2526401f99554edde291b92807c60744
Aditya-k-11 <adityakanojia.ad@gmail.com>
2026-05-23 14:13:54 +0530

    Fixed python service URL for Render

--- backend/src/embed.js
+++ backend/src/embed.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const EMBED_URL = "http://127.0.0.1:8000/embed";
+const EMBED_URL = process.env.PYTHON_SERVICE_URL || "http://127.0.0.1:8000/embed";
 
 export const getEmbedding = async (text) => {
   const res = await axios.get(EMBED_URL, {
