36dba7907026bbb5300b512b208f990da2a34d18
Aditya-k-11 <adityakanojia.ad@gmail.com>
2026-05-23 21:31:40 +0530

    chore: remove unused b2url.js

--- backend/src/b2url.js
+++
@@ -1,4 +0,0 @@
-export const getB2Url = (b2_key) => {
-  const baseUrl = process.env.B2_DOWNLOAD_URL || "https://f005.backblazeb2.com";
-  return `${baseUrl}/file/${process.env.B2_BUCKET_NAME}/${b2_key}`;
-};
